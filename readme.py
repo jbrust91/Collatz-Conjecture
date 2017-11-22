5c4ed4a88b8e088790a5c7edef2af9b8a76fdde0
Jordan <jordanrust@gmail.com>
2017-11-22 01:03:29 -0800

    README
    Added a readme

---
+++ readme.py
@@ -0,0 +1,6 @@
+Reproduce the Collatz Conjecture, which
+states that you can take any number, n,
+and if even, divide by 2, but if odd, 
+multiply by 3 and add 1, you will 
+eventually fall into a loop of 4, 2, 1.
+"""
